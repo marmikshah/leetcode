555d47054233d452fde963a35f723e006e1dc0df
Marmik Shah <marmikshah@icloud.com>
2023-01-02 20:13:33 +0800

    Implemented 1_Two_Sum

---
+++ 1_twosum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    std::vector<int> result;
+    if (nums.size() < 2) return result;
+    std::unordered_map<int, int> cache;
+    for (int i = 0; i < nums.size(); i++) {
+      int item = nums[i];
+      int difference = target - item;
+      if (cache.find(difference) == cache.end()) {
+        cache[item] = i;
+      } else {
+        // Item found
+        result.push_back(cache[difference]);
+        result.push_back(i);
+        break;
+      }
+    }
+    return result;
+  }
+};
