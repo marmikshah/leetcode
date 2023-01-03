d16d0aeb7a0945f7fd295a9713ccbeaa8756dad6
Marmik Shah <marmikshah@icloud.com>
2023-01-03 20:30:51 +0800

    Minor fixes

--- 2_addtwonum.cpp
+++ 2_addtwonum.cpp
@@ -26,15 +26,6 @@ void printList(ListNode* node) {
   std::cout << node->val << "";
 }
 
-void printEquation(ListNode* a, ListNode* b, ListNode* result) {
-  printList(a);
-  std::cout << " + ";
-  printList(b);
-  std::cout << " = ";
-  printList(result);
-  std::cout << std::endl;
-}
-
 class Solution {
  public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -82,24 +73,33 @@ class Solution {
   }
 };
 
+void printEquation(ListNode* a, ListNode* b) {
+  Solution* sol;
+  printList(a);
+  std::cout << " + ";
+  printList(b);
+  std::cout << " = ";
+  printList(sol->addTwoNumbers(a, b));
+  std::cout << std::endl;
+}
+
 int main() {
-  Solution sol;
 
   // Test Case 1
   // 342 + 465 = 807
   ListNode* case1a = new ListNode(2, new ListNode(4, new ListNode(3)));
   ListNode* case1b = new ListNode(5, new ListNode(6, new ListNode(4)));
-  printEquation(case1a, case1b, sol.addTwoNumbers(case1a, case1b));
+  printEquation(case1a, case1b);
 
   // Test Case 2
   // 9 + 9 = 18
   ListNode* case2a = new ListNode(9);
   ListNode* case2b = new ListNode(9);
-  printEquation(case1a, case1b, sol.addTwoNumbers(case1a, case1b));
+  printEquation(case2a, case2b);
 
   // Test Case 3
   // 99 + 9 = 108
   ListNode* case3a = new ListNode(9, new ListNode(9));
   ListNode* case3b = new ListNode(9);
-  printEquation(case1a, case1b, sol.addTwoNumbers(case1a, case1b));
+  printEquation(case3a, case3b);
 }
